17e007b277b79d607d6f1de1aed59a8e04b48b3e
DHARMENDER SHAKYA <110955017+DharmenderShakya@users.noreply.github.com>
2026-04-08 11:37:11 +0530

    Add files via upload

---
+++ spring_security_oauth2_practicle1/src/test/java/com/Spring_Security_oauth2_practicle1/TestSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.Spring_Security_oauth2_practicle1;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@TestConfiguration
+@EnableMethodSecurity
+ class TestSecurityConfig {
+}
